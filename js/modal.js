d1f00973eaefb7504c3a5cc927b18f81fe85cd69
fe2oz <93234748+fe2oz@users.noreply.github.com>
2021-12-01 22:39:00 +0900

    12.01/22:38

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,154 @@
 
 $(function(){
     $('.two_section_box').click(function(){
-        $('.two_section_box_modal').show();
+        $('.modal1').show();
         $('body').css("overflow", "hidden");
     })
 
     $('#cancle').click(function(){
-        $('.two_section_box_modal').hide();
+        $('.modal1').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box2').click(function(){
+        $('.modal2').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle2').click(function(){
+        $('.modal2').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box3').click(function(){
+        $('.modal3').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle3').click(function(){
+        $('.modal3').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box4').click(function(){
+        $('.modal4').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle4').click(function(){
+        $('.modal4').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box5').click(function(){
+        $('.modal5').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle5').click(function(){
+        $('.modal5').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box6').click(function(){
+        $('.modal6').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle6').click(function(){
+        $('.modal6').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box7').click(function(){
+        $('.modal7').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle7').click(function(){
+        $('.modal7').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box8').click(function(){
+        $('.modal8').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle8').click(function(){
+        $('.modal8').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box9').click(function(){
+        $('.modal9').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle9').click(function(){
+        $('.modal9').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box10').click(function(){
+        $('.modal10').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle10').click(function(){
+        $('.modal10').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box11').click(function(){
+        $('.modal11').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle11').click(function(){
+        $('.modal11').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box12').click(function(){
+        $('.modal12').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle12').click(function(){
+        $('.modal12').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box13').click(function(){
+        $('.modal13').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle13').click(function(){
+        $('.modal13').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box14').click(function(){
+        $('.modal14').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle14').click(function(){
+        $('.modal14').hide();
+        $('body').css("overflow", "scroll");
+    })
+
+    $('.two_section_box15').click(function(){
+        $('.modal15').show();
+        $('body').css("overflow", "hidden");
+    })
+
+    $('#cancle15').click(function(){
+        $('.modal15').hide();
         $('body').css("overflow", "scroll");
     })
 })
+
+
--- js/slide_choice.js
+++ js/slide_choice.js
@@ -1,2 +1,12 @@
 
 
+function simpleParallax() {
+
+    var scrolled = $(window).scrollTop() + 1;
+    
+    $('.scroll').css('background-position', '0' + -(scrolled * 0.3) + 'px');
+}
+
+$(window).scroll(function (e) {
+    simpleParallax();
+});
