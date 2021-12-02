14adbcfc2607595e32155a80ef6736a41aa0d7c8
fe2oz <93234748+fe2oz@users.noreply.github.com>
2021-12-02 17:38:56 +0900

    12.02/17:38

--- js/slide_choice.js
+++ js/slide_choice.js
@@ -1,9 +1,7 @@
 
 
 function simpleParallax() {
-
     var scrolled = $(window).scrollTop() + 1;
-    
     $('.scroll').css('background-position', '0' + -(scrolled * 0.3) + 'px');
 }
 
@@ -11,31 +9,11 @@ $(window).scroll(function (e) {
     simpleParallax();
 });
 
-$(document).ready(function(){       
-
-    var scroll_pos = 0;
-
-    $(document).scroll(function() { 
-
-        scroll_pos = $(this).horizontalSlide();
-
-        if(scroll_pos > 350) {
-
-            $("body").css('background-color', 'blue');
-
-        } else {
-
-            $("body").css('background-color', 'red');
-
-        }
-
-    });
-
-});
 
 $(function(){
     $("#slider").on('mousewheel',function(e){
         var wheelDelta = e.originalEvent.wheelDelta;
+        var sL = e.originalEvent.scrollLeft;
     
         if(wheelDelta > 0){
             console.log("up");
@@ -47,3 +25,17 @@ $(function(){
             }
         });
 });
+
+$(function(){
+    $('.show_text h1').css('transform', 'translateX(0%)');
+    $('.show_text h2').css('transform', 'translateX(0%)');
+    $('.show_text h3').css('transform', 'translateX(0%)');
+    $('.show_text > div').css('transform', 'translateX(0%)');
+})
+
+
+
+
+
+
+  
