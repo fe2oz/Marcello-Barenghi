9b692c46b419a7043d1f04c790c95dc4b93ec677
fe2oz <93234748+fe2oz@users.noreply.github.com>
2021-12-02 14:02:22 +0900

    12.02/14:02

--- js/slide_choice.js
+++ js/slide_choice.js
@@ -10,3 +10,40 @@ function simpleParallax() {
 $(window).scroll(function (e) {
     simpleParallax();
 });
+
+$(document).ready(function(){       
+
+    var scroll_pos = 0;
+
+    $(document).scroll(function() { 
+
+        scroll_pos = $(this).horizontalSlide();
+
+        if(scroll_pos > 350) {
+
+            $("body").css('background-color', 'blue');
+
+        } else {
+
+            $("body").css('background-color', 'red');
+
+        }
+
+    });
+
+});
+
+$(function(){
+    $("#slider").on('mousewheel',function(e){
+        var wheelDelta = e.originalEvent.wheelDelta;
+    
+        if(wheelDelta > 0){
+            console.log("up");
+            $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
+        }
+        else{
+            console.log("down");
+            $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
+            }
+        });
+});
