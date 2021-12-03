8de142aa9c9d9642f22e1ee44882be0e0b42f294
fe2oz <93234748+fe2oz@users.noreply.github.com>
2021-12-03 19:09:45 +0900

    12.03/19:09

--- js/header.js
+++ js/header.js
@@ -1,15 +1,1 @@
 
-
-$(function(){
-    $('.main2_large').click(function(){
-        $('#main2_small').slideToggle();
-    })
-
-    $('.main3_large').click(function(){
-        $('#main3_small').slideToggle();
-    })
-
-    $('.main4_large').click(function(){
-        $('#main4_small').slideToggle();
-    })
-})
--- js/slide_choice.js
+++ js/slide_choice.js
@@ -9,23 +9,6 @@ $(window).scroll(function (e) {
     simpleParallax();
 });
 
-
-$(function(){
-    $("#slider").on('mousewheel',function(e){
-        var wheelDelta = e.originalEvent.wheelDelta;
-        var sL = e.originalEvent.scrollLeft;
-    
-        if(wheelDelta > 0){
-            console.log("up");
-            $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
-        }
-        else{
-            console.log("down");
-            $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
-            }
-        });
-});
-
 $(function(){
     $('.show_text h1').css('transform', 'translateX(0%)');
     $('.show_text h2').css('transform', 'translateX(0%)');
